279c877360b00d0d3ba8398f7628cc7944e376c8
Özgür Güngör <168583984+ozgurgungor1@users.noreply.github.com>
2024-05-20 00:40:49 +0300

    Add files via upload

---
+++ kotlinlessons/HelloKotlin.kt
@@ -0,0 +1,23 @@
+package com.ozgur.kotlinlessons
+
+fun main(){
+
+    //print("Hello Kotlin")
+    //print("Hello Word")
+
+    println("Hello")
+    println("Kotlin")
+
+    println(3434)
+    println(3+3)
+
+
+    
+    /*
+
+    buda yorum
+
+
+     */
+
+}
---
+++ kotlinlessons/main.kt
@@ -0,0 +1,22 @@
+package com.ozgur.kotlinlessons
+
+fun main(){
+
+    print("Hello Kotlin")
+
+    print("Hello Word")
+
+    //kotlin öğreniyorum
+
+
+
+
+
+    /*
+
+    buda yorum
+
+
+     */
+
+}
